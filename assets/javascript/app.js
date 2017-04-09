5c6ac580258615d43334f0947539d5c162bac9f3
djrloria <djrloria@gmail.com>
2017-04-09 17:43:16 -0500

    0543PM 4/9

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,16 +6,54 @@ $(document).ready(function(){
   var i = 0;
   var correctAnswer;
   var gaming = false;
+  var myTimer;
+  var myTimer2;
+  var starter = true;
 
-  $.ajax({
-    url: "https://opentdb.com/api.php?amount=50&type=multiple&category=9",
-    type: "GET",
-    success: function(response) {
-      questions = response;
-      console.log(response);
-      theGame();
+  $("#start").on("click", function() {
+    if (starter) {
+      starter = false;
+      $("#start").html("Restart");
+      APIstart();
+    }
+    else {
+      questions = {};
+      wipeOut(); 
+      starter = true;
+      $("#start").html("Start");
+      $("#question, .answer").empty();
     }
   });
+
+  function wipeOut() {
+    wins = 0;
+    losses = 0;
+    timedOut = 0;
+    i = 0;
+    gaming = false;
+
+    $("#question, .answer").removeClass("hide");
+    $("#gifBox").addClass("hide");
+
+    clearTimeout(myTimer2);
+    clearTimeout(myTimer);
+    $("#answerBox").html("Answer: ");
+    $("#timerBox").html("Timer: 15");
+    $("#winBox").html("Wins: 0");
+    $("#lossBox").html("Losses: 0");
+    $("#timedOutBox").html("Timed Out: 0");
+  };
+
+  function APIstart() {
+    $.ajax({
+      url: "https://opentdb.com/api.php?amount=50&type=multiple&category=9",
+      type: "GET",
+      success: function(response) {
+        questions = response;
+        theGame();
+      }
+    });
+  };
 
   function theGame() {
     gaming = true;
@@ -50,14 +88,15 @@ $(document).ready(function(){
       if (guess === correctAnswer) {
         wins++;
         $("#msgBox").html("Good Job!");
+        $("#gifBox").attr("src", "https://media.giphy.com/media/11clOWGCHzWG7C/giphy.gif");
         reset();
       }
       else if (guess !== correctAnswer) {
         losses++;
         $("#msgBox").html("Wrong Answer!");
+        $("#gifBox").attr("src", "https://media.giphy.com/media/xTcnTehwgRcbgymhTW/giphy.gif");
         reset();
-      }
-      console.log("Wins: " + wins + " Losses: " + losses);
+      }      
     }
   });         
 
@@ -70,11 +109,13 @@ $(document).ready(function(){
     i++;
     clearTimeout(myTimer);
     $("#answerBox").html("Answer: " + correctAnswer);
-    $("#timerBox").html("10");
     $("#winBox").html("Wins: " + wins);
     $("#lossBox").html("Losses: " + losses);
     $("#timedOutBox").html("Timed Out: " + timedOut);
     $("#timerBox").html("New Question in 5")
+
+    $("#question, .answer").addClass("hide");
+    $("#gifBox").removeClass("hide");
 
     timerGame = 4;
     myTimer2 = setInterval (function() { 
@@ -82,10 +123,12 @@ $(document).ready(function(){
       timerGame--;
       if (timerGame === -1) {
         theGame();
+        $("#question, .answer").removeClass("hide");
+        $("#gifBox").addClass("hide");
         clearTimeout(myTimer2);
       }
     }, 1000);              
-   }      
+   };  
 }); 
 
 
